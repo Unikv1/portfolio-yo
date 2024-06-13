d915eb2a9aff0dab6d1043cbb997da50807b98a8
Unikv1 <facundoduenas1@gmail.com>
2024-06-13 19:09:22 -0300

    Finished first pass of navbar, footer inside of base.html. Pushed to master

---
+++ start_livereload.ps1
@@ -0,0 +1,9 @@
+# Navigate to your Django project directory
+cd $PSScriptRoot
+
+# Activate the virtual environment (if you are using one)
+.\env\Scripts\activate.ps1  # Uncomment this line if you are using a virtual environment
+
+
+# Start the Django development server
+python manage.py livereload
---
+++ start_runserver.ps1
@@ -0,0 +1,10 @@
+# Navigate to your Django project directory
+cd $PSScriptRoot
+
+# Activate the virtual environment (if you are using one)
+.\env\Scripts\activate.ps1  # Uncomment this line if you are using a virtual environment
+
+
+# Start the Django development server
+python manage.py runserver
+pause
